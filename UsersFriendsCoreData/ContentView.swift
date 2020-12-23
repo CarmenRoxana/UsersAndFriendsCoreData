8218c13ca613d90799a14fbae33d6a5796266244
Carmen Roxana <37681533+CarmenRoxana@users.noreply.github.com>
2020-12-23 14:23:30 -0800

    Initial Commit

---
+++ UsersFriendsCoreData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UsersFriendsCoreData
+//
+//  Created by Carmen Kakarash on 12/23/20.
+//  Copyright © 2020 CarmenK. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
